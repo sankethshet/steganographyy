14d19d930cc6e5c179d19ff8be918a60dcaef2df
sanketh shet <80892267+sankethshet@users.noreply.github.com>
2021-07-17 00:10:54 +0530

    Add files via upload
    this is an project that we can hide an image inside in a image

---
+++ Steganography/steganography.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import threading
+from PIL import Image
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(348, 105)
+        self.progressBar = QtWidgets.QProgressBar(Dialog)
+        self.progressBar.setGeometry(QtCore.QRect(30, 30, 291, 21))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Processing..."))
+
+
+app = QtWidgets.QApplication(sys.argv)
+Dialog = QtWidgets.QDialog()
+ui = Ui_Dialog()
+ui.setupUi(Dialog)
+
+
+class Steganography(object):
+    @staticmethod
+    def __int_to_bin(rgb):
+        """Convert an integer tuple to a binary (string) tuple.
+
+        :param rgb: An integer tuple (e.g. (220, 110, 96))
+        :return: A string tuple (e.g. ("00101010", "11101011", "00010110"))
+        """
+        r, g, b = rgb
+        return ('{0:08b}'.format(r),
+                '{0:08b}'.format(g),
+                '{0:08b}'.format(b))
+
+    @staticmethod
+    def __bin_to_int(rgb):
+        """Convert a binary (string) tuple to an integer tuple.
+
+        :param rgb: A string tuple (e.g. ("00101010", "11101011", "00010110"))
+        :return: Return an int tuple (e.g. (220, 110, 96))
+        """
+        r, g, b = rgb
+        return (int(r, 2),
+                int(g, 2),
+                int(b, 2))
+
+    @staticmethod
+    def __merge_rgb(rgb1, rgb2):
+        """Merge two RGB tuples.
+
+        :param rgb1: A string tuple (e.g. ("00101010", "11101011", "00010110"))
+        :param rgb2: Another string tuple
+        (e.g. ("00101010", "11101011", "00010110"))
+        :return: An integer tuple with the two RGB values merged.
+        """
+        r1, g1, b1 = rgb1
+        r2, g2, b2 = rgb2
+        rgb = (r1[:4] + r2[:4],
+               g1[:4] + g2[:4],
+               b1[:4] + b2[:4])
+        return rgb
+
+    @staticmethod
+    def merge(img1, img2, filename):
+        """Merge two images. The second one will be merged into the first one.
+
+        :param img1: First image
+        :param img2: Second image
+        :return: A new merged image.
+        """
+        # Check the images dimensions
+        if img2.size[0] > img1.size[0] or img2.size[1] > img1.size[1]:
+            raise ValueError('Image 2 should not be larger than Image 1!')
+
+        # Get the pixel map of the two images
+        pixel_map1 = img1.load()
+        pixel_map2 = img2.load()
+
+        ui.progressBar.setValue(20)
+        # Create a new image that will be outputted
+        new_image = Image.new(img1.mode, img1.size)
+        pixels_new = new_image.load()
+
+        for i in range(img1.size[0]):
+            for j in range(img1.size[1]):
+                ui.progressBar.setValue(88)
+                rgb1 = Steganography.__int_to_bin(pixel_map1[i, j])
+
+                # Use a black pixel as default
+                rgb2 = Steganography.__int_to_bin((0, 0, 0))
+
+                # Check if the pixel map position is valid for the second image
+                if i < img2.size[0] and j < img2.size[1]:
+                    rgb2 = Steganography.__int_to_bin(pixel_map2[i, j])
+
+                # Merge the two pixels and convert it to a integer tuple
+                rgb = Steganography.__merge_rgb(rgb1, rgb2)
+
+                pixels_new[i, j] = Steganography.__bin_to_int(rgb)
+
+        ui.progressBar.setValue(100)
+        filename = 'C:\\Users\\Sanketh\\Desktop\\' + filename
+        new_image.save(filename)
+        Dialog.close()
+        return new_image
+
+    @staticmethod
+    def unmerge(img, filename):
+        """Unmerge an image.
+
+        :param img: The input image.
+        :return: The unmerged/extracted image.
+        """
+
+        # Load the pixel map
+        pixel_map = img.load()
+        ui.progressBar.setValue(20)
+        # Create the new image and load the pixel map
+        new_image = Image.new(img.mode, img.size)
+        pixels_new = new_image.load()
+
+        # Tuple used to store the image original size
+        original_size = img.size
+        ui.progressBar.setValue(30)
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                # Get the RGB (as a string tuple) from the current pixel
+                r, g, b = Steganography.__int_to_bin(pixel_map[i, j])
+                ui.progressBar.setValue(66)
+                # Extract the last 4 bits (corresponding to the hidden image)
+                # Concatenate 4 zero bits because we are working with 8 bit
+                rgb = (r[4:] + '0000',
+                       g[4:] + '0000',
+                       b[4:] + '0000')
+
+                # Convert it to an integer tuple
+                pixels_new[i, j] = Steganography.__bin_to_int(rgb)
+
+                # If this is a 'valid' position, store it
+                # as the last valid position
+                if pixels_new[i, j] != (0, 0, 0):
+                    original_size = (i + 1, j + 1)
+
+        # Crop the image based on the 'valid' pixels
+        ui.progressBar.setValue(100)
+        new_image = new_image.crop((0, 0, original_size[0], original_size[1]))
+        filename = 'C:\\Users\\Sanketh\\Desktop\\' + filename
+        Dialog.close()
+        new_image.save(filename + ".png")
+        return new_image
+
+
+def merge(img1, img2, filename):
+    t1 = threading.Thread(target=Steganography.merge, args=(Image.open(img1), Image.open(img2), filename))
+    t1.start()
+    Dialog.show()
+
+
+def unmerge(img, filename):
+    t2 = threading.Thread(target=Steganography.unmerge, args=(Image.open(img), filename))
+    t2.start()
+    Dialog.show()
